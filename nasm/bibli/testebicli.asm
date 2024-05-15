45516b461abc202a6c9e3440cabab06dbaa8bb0a
poledna0 <henrique.poledna@gmail.com>
2024-05-15 14:46:47 -0300

    feat: bibliotecafim

---
+++ nasm/bibli/testebicli.asm
@@ -0,0 +1,32 @@
+%include '/home/henrique/vscode/assembly-2/nasm/bibli/biblioteca.inc'
+
+section .text
+
+global _start
+
+_start:
+    
+  lea esi, [BUFFER]             ; Load Effective Address
+  add esi, 0x9
+  mov byte[esi], 0xA
+
+  dec esi
+  mov dl, 0x13                  ; -> 'C' 0x33 -> 'c'
+  add dl, '0'
+  mov [esi], dl
+
+  dec esi
+  mov dl, 0x12                  ; -> 'B' 0x32 -> 'b'
+  add dl, '0'
+  mov [esi], dl
+
+  dec esi
+  mov dl, 0x11                  ; -> 'A' 0x31 -> 'a'
+  add dl, '0'
+  mov [esi], dl
+
+  call saidaResultado
+
+  mov eax, SYS_EXIT
+  mov ebx, RET_EXIT
+  int SYS_CALL
