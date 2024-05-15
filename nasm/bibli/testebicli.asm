e8797f9735651e601c5f16ac7438e15c66c22758
poledna0 <henrique.poledna@gmail.com>
2024-05-15 15:10:24 -0300

    feat: fim bibl

--- nasm/bibli/testebicli.asm
+++ nasm/bibli/testebicli.asm
@@ -6,24 +6,24 @@ global _start
 
 _start:
 
-    lea esi, [BUFFER]             ; Load Effective Address
-    add esi, 0x9
-    mov byte[esi], 0xA
+    lea ESI, [BUFFER]             ; Load Effective Address ESI amrazena o endereço onde esta o buffer, [] para indicar que esttamos nos referindo a um endereço de uma variavel
+    add ESI, 0x9                  ;fim de uma string
+    mov byte[ESI], 0xA
 
-    dec esi
+    dec ESI
     mov dl, 0x13                  ; -> 'C' 0x33 -> 'c'
     add dl, '0'
-    mov [esi], dl
+    mov [ESI], dl
 
-    dec esi
+    dec ESI
     mov dl, 0x12                  ; -> 'B' 0x32 -> 'b'
     add dl, '0'
-    mov [esi], dl
+    mov [ESI], dl
 
-    dec esi
+    dec ESI
     mov dl, 0x11                  ; -> 'A' 0x31 -> 'a'
     add dl, '0'
-    mov [esi], dl
+    mov [ESI], dl
 
     call saidaResultado
 
